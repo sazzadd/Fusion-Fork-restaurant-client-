b8b6a070c935754be8def33b8bd881760dbf0b1a
sazzad <sazzadador@gmail.com>
2024-12-30 13:40:10 +0600

    added cover darkOverlay

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import menuImg from "../../assets/menu/menu-bg.png";
 import Cover from "../../shared/Cover";
+import PopularMenu from "../PopularMenu";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Fusion Fork | Menu</title>
       </Helmet>
-      
-      <Cover img={"https://i.ibb.co.com/5hFyPW4/herro-slide-1.jpg"}></Cover>
+
+      <Cover
+        title="Our Menu"
+        subTitle="Would you like to try a dish?"
+        img={"https://i.ibb.co.com/5hFyPW4/herro-slide-1.jpg"}
+      ></Cover>
+      <div className="py-5">
+        <PopularMenu></PopularMenu>
+      </div>
+      <Cover
+        title="DESSERTS"
+        subTitle="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        img={"https://i.ibb.co.com/Fz7mBx1/header-how-to-design-a-balanced-dessert-menu.jpg"}
+      ></Cover>
+      <div className="py-5">
+        <PopularMenu></PopularMenu>
+      </div>
     </div>
   );
 };
--- src/shared/Cover.jsx
+++ src/shared/Cover.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Cover = ({ img }) => {
+const Cover = ({ img, title, subTitle }) => {
   return (
     <div>
       <div
-        className="hero min-h-screen"
+        className="hero h-[560px]"
         style={{
           backgroundImage: `url("${img}")`,
           backgroundSize: "cover",
@@ -13,14 +13,9 @@ const Cover = ({ img }) => {
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+          <div className="max-w-lg bg-black opacity-40 px-16 py-10">
+            <h1 className="mb-5 text-5xl text-white font-bold">{title}</h1>
+            <p className="mb-5">{subTitle}</p>
           </div>
         </div>
       </div>
