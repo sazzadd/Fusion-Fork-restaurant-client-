65745400d396dcc18815f706a24892fd3b5d1d1b
sazzad <sazzadador@gmail.com>
2024-12-30 19:19:18 +0600

    crate foodCard.jsx

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,9 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../hooks/useMenu";
+import Cover from "../../shared/Cover";
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category == "dessert");
+  const pizza = menu.filter((item) => item.category == "pizza");
+  const soup = menu.filter((item) => item.category == "soup");
+  const salad = menu.filter((item) => item.category == "salad");
+  const offered = menu.filter((item) => item.category == "offered");
   return (
     <div>
-      <h1>Order</h1>
+      <Cover
+        img="https://i.ibb.co.com/pRFz1pM/17091737-spencer-davis-w-gshci0cr8-unsplash-2-2-cover-2000x1250.webp"
+        title="OUR SHOP"
+        subTitle="would you like  to order our ish"
+      ></Cover>
+      <div>
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            <Tab>Pizza</Tab>
+            <Tab>Salad</Tab>
+            <Tab>Soup</Tab>
+            <Tab>Dessert</Tab>
+            <Tab>Drinks</Tab>
+          </TabList>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
