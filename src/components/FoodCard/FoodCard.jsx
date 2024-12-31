c7d106fb7131922bdee4b3f159ea4b603e111273
sazzad <sazzadador@gmail.com>
2024-12-31 15:01:14 +0600

    improve foodcard design

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,20 +2,30 @@ import React from "react";
 
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe } = item;
+
   return (
-    <div>
-      <div className="bg-base-100 shadow-xl relative">
+    <div className="max-w-sm mx-auto rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
+      <div className="relative">
         <figure>
-          <img src={image} className="w-full" alt="Shoes" />
-          <p className="right-0 top-0  absolute p-3 font-bold bg-slate-700 text-white">{price} $</p>
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-56 object-cover transition-transform duration-300 transform hover:scale-105"
+          />
+          <p className="absolute top-2 right-2 px-3 py-1 bg-black bg-opacity-70 text-white text-sm rounded-md font-semibold">
+            ${price}
+          </p>
         </figure>
-        <div className="card-body items-center bg-cover text-center">
-          
-          <h2 className="card-title">{name}</h2>
-          <p cl>{recipe}</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+      </div>
+      <div className="p-5 bg-gradient-to-r from-gray-100 via-white to-gray-100">
+        <h2 className="text-lg font-bold text-gray-800 mb-2 text-center">
+          {name}
+        </h2>
+        <p className="text-sm text-gray-600 text-center mb-4">{recipe}</p>
+        <div className="flex justify-center">
+          <button className="btn btn-outline border-0 border-b-4 border-[#BB8506] mb-8 mt-4 text-black hover:text-white hover:bg-black hover:border-black transform hover:scale-105 transition-all duration-300 ease-in-out">
+            Order Now
+          </button>
         </div>
       </div>
     </div>
