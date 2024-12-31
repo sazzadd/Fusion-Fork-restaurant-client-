d1b286982a3b682b51b353166bc5547bea0eed15
sazzad <sazzadador@gmail.com>
2024-12-31 19:45:56 +0600

    fix design issue

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -7,8 +7,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
 import { Navigation } from "swiper/modules";
 // Import Swiper styles
+import { Rating } from "@smastrom/react-rating";
 import "swiper/css";
 import "swiper/css/navigation";
+
+import "@smastrom/react-rating/style.css";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
@@ -24,11 +28,20 @@ const Testimonial = () => {
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
           {reviews.map((review) => (
             <SwiperSlide key={review._id}>
-              <div className="m-24">
+              <div className="flex flex-col items-center m-24">
+                <div>
+                  <Rating
+                    style={{ maxWidth: 180 }}
+                    value={review.rating}
+                    readOnly
+                  />
+                </div>
+                <FaQuoteLeft className="text-3xl  text-gray-500 mx-auto mt-5 mb-4" />
                 <p className="text-center py-4">{review.details}</p>
-                <h3 className="text-2xl text-center text-orange-400">{review.name}</h3>
+                <h3 className="text-2xl text-center text-orange-400">
+                  {review.name}
+                </h3>
               </div>
-             
             </SwiperSlide>
           ))}
         </Swiper>
--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -2,11 +2,39 @@ import React from 'react';
 import SecTitile from '../components/SecTitile';
 
 const Testimonials = () => {
-    return (
-        <div>
-            <SecTitile></SecTitile>
-        </div>
-    );
+    const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/reviews")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div>
+      <SecTitile heading="TESTIMONIAL"></SecTitile>
+      <div>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col items-center m-24">
+                <div>
+                  <Rating
+                    style={{ maxWidth: 180 }}
+                    value={review.rating}
+                    readOnly
+                  />
+                </div>
+
+                <p className="text-center py-4">{review.details}</p>
+                <h3 className="text-2xl text-center text-orange-400">
+                  {review.name}
+                </h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
