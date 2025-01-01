520ad03ece5c3391ea073c093006f190b585d1b4
sazzad <sazzadador@gmail.com>
2025-01-01 12:32:00 +0600

    auth Context login logout crate use

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,47 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "./../firebase/firebase.config";
 const AuthContext = createContext(null);
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authInfo = {
     user,
     loading,
+    createNewUser,
+    userLogin,
+    logOut,
   };
 
   return (
