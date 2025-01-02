70e602baa5f4aa21bb98f9a251d1aa2dacfa0aa2
sazzad <sazzadador@gmail.com>
2025-01-02 21:04:10 +0600

    tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
@@ -8,25 +9,17 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import AuthProvider from "./provider/AuthProvider";
 import router from "./Routes/Router";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
-const queryClient = new QueryClient()
-
-
-
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <ToastContainer></ToastContainer>
-    <HelmetProvider>
-      <React.StrictMode>
-        <ToastContainer></ToastContainer>
-
-        <RouterProvider router={router} />
-      </React.StrictMode>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </HelmetProvider>
+    </QueryClientProvider>
   </AuthProvider>
 );
