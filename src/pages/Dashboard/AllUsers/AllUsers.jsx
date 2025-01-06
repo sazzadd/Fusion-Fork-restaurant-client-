bd0b8e3380338b4887c19e50c4e8732089fde17a
sazzad <sazzadador@gmail.com>
2025-01-07 01:41:01 +0600

    use tanstack querry and fetch data from /users

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,65 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { FaTrashAlt, FaUser } from "react-icons/fa";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+  const handleDeleteUser = ()=> {
+
+  }
   return (
     <div>
-      <h1>all users </h1>
+      <h1>all users {users.length}</h1>
+      <h1></h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role </th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {users.map((user, index) => (
+              <tr key={user._id} className="bg-base-200">
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                <button
+                    className="text-red-500 bg-orange-300 p-1 rounded hover:text-red-700 transition-transform transform hover:scale-110"
+                    onClick={() => handleDeleteUser(user)}
+                  >
+                    <FaUser className="text-2xl" />
+                  </button>
+
+                </td>
+                <td>
+                  <button
+                    className="text-red-500 hover:text-red-700 transition-transform transform hover:scale-110"
+                    onClick={() => handleDeleteUser(user)}
+                  >
+                    <FaTrashAlt className="text-2xl" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
