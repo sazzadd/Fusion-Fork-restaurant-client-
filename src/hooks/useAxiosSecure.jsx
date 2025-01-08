e21cae4bd23965d96b36290a475fa602d3e9c30c
sazzad <sazzadador@gmail.com>
2025-01-08 13:24:29 +0600

    navigate use in axiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
@@ -18,13 +20,15 @@ const useAxiosSecure = () => {
   );
   axiosSecure.interceptors.response.use(
     function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
       // Do something with response data
       return response;
     },
     (error) => {
       const status = error.response.status;
       console.log("status rrror in the interceptor", status);
+      if (status === 401 || status === 403) {
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
