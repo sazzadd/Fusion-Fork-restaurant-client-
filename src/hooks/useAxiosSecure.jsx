2a3b472b86bc5d31b1633bc76fc836088f7751e3
sazzad <sazzadador@gmail.com>
2025-01-08 13:17:47 +0600

    use interceptor in axiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,33 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
-})
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request sttoped by interceptor");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    (error) => {
+      const status = error.response.status;
+      console.log("status rrror in the interceptor", status);
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
