483c7a38d3aeabefcdfa68341f17d85ec426caee
sazzad <sazzadador@gmail.com>
2025-01-10 00:16:21 +0600

    confirm alert in manage item table

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -6,7 +6,23 @@ import { FaEdit, FaTrashAlt } from "react-icons/fa";
 const ManageItems = () => {
   const [menu] = useMenu();
   const  handleDelete = (item )=> {
-
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+        //   Swal.fire({
+        //     title: "Deleted!",
+        //     text: "Your file has been deleted.",
+        //     icon: "success"
+        //   });
+        }
+      });
   }
   return (
    
