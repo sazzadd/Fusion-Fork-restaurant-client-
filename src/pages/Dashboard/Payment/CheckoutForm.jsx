fcbb179d5580bc58762174309c6ac478c0da336a
sazzad <sazzadador@gmail.com>
2025-01-11 00:39:37 +0600

    stripe.js

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import { useElements, useStripe } from "@stripe/react-stripe-js";
+import React from "react";
 
 const CheckoutForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+     
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    
+    if (card === null) {
+        return;
+      }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CheckoutForm;
