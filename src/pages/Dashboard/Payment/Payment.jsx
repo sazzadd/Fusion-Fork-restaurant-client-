c6a418796036780a3a4ded1ed3d4895f554c23c5
sazzad <sazzadador@gmail.com>
2025-01-11 00:03:00 +0600

    install react Stripe js

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import SecTitile from '../../../components/SecTitile';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 const Payment = () => {
+    const stripePromise  = loadStripe('');
     return (
         <div>
             <SecTitile heading="PAYMENT" subHeading="At a Glance!"></SecTitile>
+            <div>
+                <Elements stripe={stripePromise}>
+                    
+                </Elements>
+            </div>
         </div>
+        
     );
 };
 
